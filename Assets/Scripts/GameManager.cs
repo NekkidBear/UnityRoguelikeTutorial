97acdc3113c08e016d8b121ddcb5954e23c4bd62
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-07-23 17:34:46 -0500

    update board manager and game manager to use Scene management

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using Assets.Scripts;
 using System;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
