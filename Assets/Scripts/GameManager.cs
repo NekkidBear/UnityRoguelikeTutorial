3cf32d943084faad9979fc5fa02fde0dbdc2f998
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-07-23 12:17:43 -0500

    Stop tracking the Library directory

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+using Assets.Scripts;
+using System;
+
+public class GameManager : MonoBehaviour 
+{
+    public static GameManager instance = null;
+    public BoardManager boardScript;
+
+    private int level = 3;
+
+    void Awake ()
+    {   
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    void InitGame ()
+    {
+        boardScript.SetupScene(level);
+    }
+}
