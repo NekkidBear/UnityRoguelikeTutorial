dafbb5aadb14bbf810d9bd2fdc1c07f08129eb7f
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-07-23 13:27:50 -0500

    fix large file tracking

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,26 +3,26 @@ using System.Collections;
 using Assets.Scripts;
 using System;
 
-public class GameManager : MonoBehaviour 
+public class GameManager : MonoBehaviour
 {
     public static GameManager instance = null;
     public BoardManager boardScript;
 
     private int level = 3;
 
-    void Awake ()
-    {   
+    void Awake()
+    {
         if (instance == null)
             instance = this;
         else if (instance != this)
             Destroy(gameObject);
-        
+
         DontDestroyOnLoad(gameObject);
         boardScript = GetComponent<BoardManager>();
         InitGame();
     }
 
-    void InitGame ()
+    void InitGame()
     {
         boardScript.SetupScene(level);
     }
---
+++ Assets/Scripts/MovingObject.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MovingObject : MonoBehaviour
+{
+    public float moveTime = 0.1f;
+    public LayerMask blockingLayer;
+
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rb2d;
+    private float inverseMoveTime;
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        rb2d = GetComponent<Rigidbody2D>();
+        inverseMoveTime = 1f / moveTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
